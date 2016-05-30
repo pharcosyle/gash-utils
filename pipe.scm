de319e38eb4efc9a4cfd7783bea2c0a69664f81b
Rutger van Beusekom <rutger.van.beusekom@gmail.com>
2016-05-31 00:13:04 +0200

    Add pipeline support.

---
+++ pipe.scm
@@ -0,0 +1,53 @@
+(use-modules (ice-9 popen))
+(use-modules (srfi srfi-8)) ;; receive
+
+(define (pipe*)
+  (let ((p (pipe)))
+    (values (car p) (cdr p))))
+
+;;              lhs         rhs
+;; [source] w -> r [filter] w -> r [sink]
+
+(define (exec* command)
+  (apply execlp (cons (car command) command)))
+
+(define (spawn-source command)
+  (receive (r w) (pipe*)
+    (let ((pid (primitive-fork)))
+      (cond ((= 0 pid) (close r)
+             (move->fdes w 1)
+             (exec* command))
+            (#t
+             (close w)
+             r)))))
+
+(define (spawn-filter src command)
+  (receive (r w) (pipe*)
+    (let ((pid (primitive-fork)))
+      (cond ((= 0 pid)
+             (move->fdes src 0)
+             (close r)
+             (move->fdes w 1)
+             (exec* command))
+            (#t
+             (close w)
+             r)))))
+
+(define (spawn-sink src command)
+  (let ((pid (primitive-fork)))
+    (cond ((= 0 pid)
+           (move->fdes src 0)
+           (exec* command))
+          (#t
+           (close src)
+           (waitpid pid)))))
+
+(define (pipeline commands)
+  (if (< 1 (length commands))
+      (let loop ((src (spawn-source (car commands)))
+                 (commands (cdr commands)))
+        (if (null? (cdr commands)) (spawn-sink src (car commands))
+            (loop (spawn-filter src (car commands))
+                  (cdr commands))))))
+
+;;(pipeline (list (list "ls" "/") (list "grep" "o") (list "tr" "o" "e")))
