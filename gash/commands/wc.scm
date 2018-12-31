faa2208d09c17d3f27d81073f0123a5887433a5d
Jan Nieuwenhuizen <janneke@gnu.org>
2018-12-31 23:02:28 +0100

    wc: Support -w.

--- gash/commands/wc.scm
+++ gash/commands/wc.scm
@@ -29,50 +29,72 @@
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-11)
 
+  #:use-module (ice-9 format)
   #:use-module (ice-9 match)
   #:use-module (gash shell-utils)
   #:export (
             wc
             ))
 
-(define (lines+chars port)
-  "Return the number of lines and number of chars read from PORT."
-  (let loop ((lines 0) (chars 0))
-    (match (read-char port)
-      ((? eof-object?)              ;done!
-       (values lines chars))
-      (#\newline                    ;recurse
-       (loop (1+ lines) (1+ chars)))
-      (_                            ;recurse
-       (loop lines (1+ chars))))))
+(define (lines+words+chars port)
+  "Return the number of lines, words and chars read from PORT."
+  (let loop ((lines 0) (words 0) (chars 0) (word? #f))
+    (let ((c (read-char port)))
+      (match c
+       ((? eof-object?)                 ;done!
+        (values lines words chars))
+       (#\newline                       ;recurse
+        (loop (1+ lines) words (1+ chars) #f))
+       ((or #\return #\space #\tab #\vtab) ;recurse
+        (loop lines words (1+ chars) #f))
+       (_                               ;recurse
+        (loop lines (if word? words (1+ words)) (1+ chars) #t))))))
 
 (define (wc-print file)
-  (let-values (((lines chars)
-                (call-with-input-file file lines+chars)))
-              (format #t "~a ~a ~a~%" lines chars file)))
+  (let-values (((lines words chars)
+                (if (string-null? file) (lines+words+chars (current-input-port))
+                    (call-with-input-file file lines+words+chars))))
+    (format #t "~7@a ~7@a ~7@a ~a~%" lines words chars file)))
 
 (define (wc-l-print file)
-  (let-values (((lines chars)
-                (call-with-input-file file lines+chars)))
-              (format #t "~a ~a~%" lines file)))
+  (let-values (((lines words chars)
+                (if (string-null? file) (lines+words+chars (current-input-port))
+                    (call-with-input-file file lines+words+chars))))
+    (format #t "~a ~a~%" lines file)))
 
 (define (wc-c-print file)
-  (let-values (((lines chars)
-                (call-with-input-file file lines+chars)))
-              (format #t "~a ~a~%" chars file)))
+  (let-values (((lines words chars)
+                (if (string-null? file) (lines+words+chars (current-input-port))
+                    (call-with-input-file file lines+words+chars))))
+    (format #t "~a ~a~%" chars file)))
+
+(define (wc-w-print file)
+  (let-values (((lines words chars)
+                (if (string-null? file) (lines+words+chars (current-input-port))
+                    (call-with-input-file file lines+words+chars))))
+    (format #t "~7@a ~a~%" words file)))
 
 (define (wc- . files)
-  (for-each wc-print (filter file-exists?* files)))
+  (if (null? files) (wc-print "")
+      (for-each wc-print (filter file-exists?* files))))
 
 (define (wc-l . files)
-  (for-each wc-l-print (filter file-exists?* files)))
+  (if (null? files) (wc-l-print "")
+   (for-each wc-l-print (filter file-exists?* files))))
 
 (define (wc-c . files)
-  (for-each wc-c-print (filter file-exists?* files)))
+  (if (null? files) (wc-c-print "")
+      (for-each wc-c-print (filter file-exists?* files))))
+
+(define (wc-w . files)
+  (if (null? files) (wc-w-print "")
+      (for-each wc-w-print (filter file-exists?* files))))
 
 (define (wc name . args)
   (cond ((member "-l" args)
          (apply wc-l (delete "-l" args)))
+        ((member "-w" args)
+         (apply wc-w (delete "-w" args)))
         ((member "-c" args)
          (apply wc-c (delete "-c" args)))
         (else
