10ef777270b422583a1902b85fd3ae2e89059861
Timothy Sample <samplet@ngyro.com>
2019-01-05 13:38:27 -0500

    Remove shell pieces.
    * gash/builtins.scm, gash/environment.scm, gash/gash.scm,
    gash/geesh.scm, gash/grammar.scm, gash/job.scm, gash/peg/cache.scm,
    gash/peg/codegen.scm, gash/peg/simplify-tree.scm,
    gash/peg/string-peg.scm, gash/peg/using-parsers.scm, gash/pipe.scm,
    gash/readline.scm, gash/script.scm, scripts/gash.in : Delete files.
    * test.sh: Remove reference to 'scripts/gash'.
    * Makefile.am: Remove shell rules.
    * .gitignore: Remove 'scripts/gash'.

--- test.sh
+++ test.sh
@@ -3,12 +3,11 @@ if [ -n "$V" ]; then
     set -x
 fi
 DIFF=${DIFF-diff}
-TEST_SHELL=${TEST_SHELL-scripts/gash}
 
 t="$1"
 b=tests/$(basename "$t" .sh)
 set +e
-timeout 10 $TEST_SHELL -e "$b".sh -s --long file0 file1 > "$b".1 2> "$b".2
+timeout 10 $SHELL -e "$b".sh -s --long file0 file1 > "$b".1 2> "$b".2
 r=$?
 set -e
 if [ -f "$b".exit ]; then
