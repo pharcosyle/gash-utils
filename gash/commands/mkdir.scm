ec383f596617449a3bc7f46b23e32305b831a488
Timothy Sample <samplet@ngyro.com>
2020-03-30 17:13:41 -0400

    mkdir: Use 'parse-options'.
    (*help-message*): New variable.
    (*version-message*): New variable.
    (*options-grammar*): New variable.
    (mkdir'): Use 'parse-options' instead of 'getopt-long'; remove help
    and version message handling.

--- gash/commands/mkdir.scm
+++ gash/commands/mkdir.scm
@@ -22,10 +22,32 @@
 ;;; Code:
 
 (define-module (gash commands mkdir)
-  #:use-module (ice-9 getopt-long)
   #:use-module (gash commands config)
   #:use-module (gash shell-utils)
+  #:use-module (gash-utils options)
   #:export (mkdir'))
+
+(define *help-message* "\
+Usage: mkdir [OPTION]... DIRECTORY...
+Create the DIRECTORY(ies), if they do not already exist.
+
+Options:
+  -m, --mode=MODE   set file mode (as in chmod), not a=rwx - umask
+  -p, --parents     no error if existing, make parent directories as needed
+  -h, --help        display this help and exit
+  -V, --version     output version information and exit
+")
+
+(define *version-message*
+  (format #f "mkdir (~a) ~a~%" %package-name %version))
+
+(define *options-grammar*
+  (make-options-grammar
+   `((value mode #\m)
+     (flag parents #\p)
+     (message ("help" #\h) ,*help-message*)
+     (message ("version" #\V) ,*version-message*))
+   #:default 'files))
 
 (define (call-with-umask mask thunk)
   (let ((outer-mask #f)
@@ -36,40 +58,18 @@
       (lambda () (set! inner-mask (umask outer-mask))))))
 
 (define (mkdir' . args)
-  (let* ((option-spec
-	  '((help (single-char #\h))
-            (mode (single-char #\m) (value #t))
-            (parents (single-char #\p))
-            (version (single-char #\V))))
-	 (options (getopt-long args option-spec))
-	 (files (option-ref options '() '()))
-         (mode (option-ref options 'mode #f))
-         (parents? (option-ref options 'parents #f))
-	 (help? (option-ref options 'help #f))
-         (version? (option-ref options 'version #f))
-         (files (option-ref options '() '()))
-         (usage? (and (not help?) (null? files))))
-    (cond (version? (format #t "mkdir (GASH) ~a\n" %version) (exit 0))
-          ((or help? usage?) (format (if usage? (current-error-port) #t)
-                                     "\
-Usage: mkdir [OPTION]... DIRECTORY...
-Create the DIRECTORY(ies), if they do not already exist.
-
-Options:
-      --help              display this help and exit
-  -m, --mode=MODE         set file mode (as in chmod), not a=rwx - umask
-  -p, --parents           no error if existing, make parent directories as needed
-      --version           output version information and exit
-
-")
-           (exit (if usage? 2 0)))
-          (else
-           (let* ((chomodifiers (parse-chmodifiers (or mode "755")))
-                  (mode (chmodifiers->mode chomodifiers)))
-             ;; XXX: This seems like a fragile way to invert the mode.
-             (call-with-umask (bit-extract (lognot mode) 0 9)
-               (lambda ()
-                 (for-each (if parents? mkdir-p (@ (guile) mkdir))
-                           files))))))))
+  (let* ((options (parse-options args *options-grammar*))
+         (mode (assoc-ref options 'mode))
+         (parents? (assoc-ref options 'parents))
+	 (files (or (assoc-ref options 'files) '())))
+    (when (null? files)
+      (format (current-error-port) "mkdir: missing operand~%")
+      (exit 2))
+    (let* ((chomodifiers (parse-chmodifiers (or mode "755")))
+           (mode (chmodifiers->mode chomodifiers)))
+      ;; XXX: This seems like a fragile way to invert the mode.
+      (call-with-umask (bit-extract (lognot mode) 0 9)
+        (lambda ()
+          (for-each (if parents? mkdir-p (@ (guile) mkdir)) files))))))
 
 (define main mkdir')
