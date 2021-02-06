45808976af91fbf0098b71dcdcee2ccec02ea0db
Timothy Sample <samplet@ngyro.com>
2021-02-06 17:54:14 -0500

    ls: Use 'parse-options'.
    This fixes a test failure on Guile 3.0.  The test removed by this
    change is not the one that was failing.  The removed test is a very
    low value one that simply runs 'ls --version'.  Now that we rely on
    the '%package-name' variable for generating the version message, this
    test is no longer reliable.
    
    * gash/commands/ls.scm (*help-message*): New variable.
    (*version-message*): New variable.
    (*options-grammar*): New variable.
    (ls): Parse options using 'parse-options'.
    * tests/core-utils.org (ls-sanity): Remove test.

--- gash/commands/ls.scm
+++ gash/commands/ls.scm
@@ -27,57 +27,17 @@
 
 (define-module (gash commands ls)
   #:use-module (ice-9 ftw)
-  #:use-module (ice-9 getopt-long)
   #:use-module (ice-9 match)
   #:use-module (srfi srfi-1)
   #:use-module (srfi srfi-26)
   #:use-module (gash commands config)
   #:use-module (gash io)
   #:use-module (gash shell-utils)
+  #:use-module (gash-utils options)
   #:export (ls))
 
-(cond-expand
- (guile
-  ;; Support -1, see https://lists.gnu.org/archive/html/bug-guile/2018-07/msg00009.html
-  (module-define! (resolve-module '(ice-9 getopt-long)) 'short-opt-rx (make-regexp "^-([a-zA-Z0-9]+)(.*)")))
- (else))
-
-(define (file-modification-time<? a b)
-  (cond ((= (stat:mtime (cdr a)) (stat:mtime (cdr b)))
-         (< (stat:mtimensec (cdr a)) (stat:mtimensec (cdr b))))
-        (else
-         (< (stat:mtime (cdr a)) (stat:mtime (cdr b))))))
-
-(define (ls . args)
-  (let* ((option-spec
-          '((all (single-char #\a))
-            (classify (single-char #\F))
-            (directory (single-char #\d))
-            (dereference (single-char #\L))
-            (inode (single-char #\i))
-            (long (single-char #\l))
-            (one-file-per-line (single-char #\1))
-            (reverse (single-char #\r))
-            (sort-by-modification-time (single-char #\t))
-
-            (help)
-            (version)))
-         (options (getopt-long args option-spec))
-         (all? (option-ref options 'all #f))
-         (classify? (option-ref options 'classify #f))
-         (directory? (option-ref options 'directory #f))
-         (inode? (option-ref options 'inode #f))
-         (long? (option-ref options 'long #f))
-         (one-file-per-line? (option-ref options 'one-file-per-line #f))
-         (reverse? (option-ref options 'reverse #f))
-         (sort-by-modification-time? (option-ref options 'sort-by-modification-time #f))
-
-         (help? (option-ref options 'help #f))
-         (version? (option-ref options 'version #f))
-         (files (option-ref options '() '()))
-         (follow-links? (not directory?)))
-    (cond (version? (format #t "ls (GASH) ~a\n" %version))
-          (help? (display "Usage: ls [OPTION]... [FILE]...
+(define *help-message* "\
+Usage: ls [OPTION]... [FILE]...
 
 Options:
   -a, --all          do not ignore entries starting with .
@@ -89,70 +49,107 @@ Options:
   -r, --reverse      reverse order while sorting
   -t                 sort by modification time, newest first
   -1                 list one file per line
-"))
-          (else
-           (let* ((stat* (if follow-links? stat lstat))
-                  (files (if (null? files)
-                             (if directory?
-                                 (list `("." . ,(stat* ".")))
-                                 (map (lambda (filename)
-                                        `(,filename . ,(stat* filename)))
-                                      (scandir ".")))
-                             (append-map (lambda (file)
-                                           (catch 'system-error
-                                             (lambda ()
-                                               (match (stat:type (stat* file))
-                                                 ('directory
-                                                  (if directory?
-                                                      (list `(,file . ,(stat* file)))
-                                                      (match (scandir file)
-                                                        (#f (list `(,file . ,(stat* file))))
-                                                        (files (map (lambda (f)
-                                                                      `(,f . ,(stat* (string-append file "/" f))))
-                                                                    files)))))
-                                                 (_ (list `(,file . ,(stat* file))))))
-                                             (lambda args
-                                               (let ((errno (system-error-errno args)))
-                                                 (format (current-error-port) "~a: ~a~%"
-                                                         file (strerror errno))
-                                                 '()))))
-                                         files)))
-                  (dot-file? (lambda (x) (let ((file (car x)))
-                                           (and (string-prefix? "." file)
-                                                (not (string-prefix? "./" file))
-                                                (not (string-prefix? "../" file))))))
-                  (files (if (or all? directory?) files
-                             (filter (negate dot-file?) files)))
-                  (files (if (not sort-by-modification-time?) files
-                             (reverse (sort files file-modification-time<?))))
-                  (files (if (not reverse?) files
-                             (reverse files))))
-             (cond (long?
-                    (for-each (match-lambda
-                                ((f . st)
-                                 (when inode?
-                                   (format #t "~a " (stat:ino st)))
-                                 (display-file f #:classify? classify?)
-                                 (newline)))
-                              files))
-                   (one-file-per-line?
-                    (for-each (match-lambda
-                                ((f . st)
-                                 (when inode?
-                                   (format #t "~a " (stat:ino st)))
-                                 (display f)
-                                 (when classify?
-                                   (display (file-class st)))
-                                 (newline)))
-                              files))
-                   (else
-                    (display-tabulated (map (match-lambda
-                                              ((f . st)
-                                               (cond (inode?
-                                                      (format #f "~a ~a" (stat:ino st) f))
-                                                     (classify?
-                                                      (format #f "~a~a" f (file-class st)))
-                                                     (else f))))
-                                            files)))))))))
+")
+
+(define *version-message*
+  (format #f "ls (~a) ~a~%" %package-name %version))
+
+(define *options-grammar*
+  (make-options-grammar
+   `((flag all #\a)
+     (flag classify #\F)
+     (flag directory #\d)
+     (flag dereference #\L)
+     (flag inode #\i)
+     (flag long #\l)
+     (flag one-file-per-line #\1)
+     (flag reverse #\r)
+     (flag sort-by-modification-time #\t)
+     (message ("help" #\h) ,*help-message*)
+     (message ("version" #\V) ,*version-message*))
+   #:default 'files))
+
+(define (file-modification-time<? a b)
+  (cond ((= (stat:mtime (cdr a)) (stat:mtime (cdr b)))
+         (< (stat:mtimensec (cdr a)) (stat:mtimensec (cdr b))))
+        (else
+         (< (stat:mtime (cdr a)) (stat:mtime (cdr b))))))
+
+(define (ls . args)
+  (let* ((options (parse-options args *options-grammar*))
+         (all? (assoc-ref options 'all))
+         (classify? (assoc-ref options 'classify))
+         (directory? (assoc-ref options 'directory))
+         (inode? (assoc-ref options 'inode))
+         (long? (assoc-ref options 'long))
+         (one-file-per-line? (assoc-ref options 'one-file-per-line))
+         (reverse? (assoc-ref options 'reverse))
+         (sort-by-modification-time? (assoc-ref options
+                                                'sort-by-modification-time))
+         (files (or (assoc-ref options 'files) '()))
+         (follow-links? (not directory?)))
+    (let* ((stat* (if follow-links? stat lstat))
+           (files (if (null? files)
+                      (if directory?
+                          (list `("." . ,(stat* ".")))
+                          (map (lambda (filename)
+                                 `(,filename . ,(stat* filename)))
+                               (scandir ".")))
+                      (append-map (lambda (file)
+                                    (catch 'system-error
+                                      (lambda ()
+                                        (match (stat:type (stat* file))
+                                          ('directory
+                                           (if directory?
+                                               (list `(,file . ,(stat* file)))
+                                               (match (scandir file)
+                                                 (#f (list `(,file . ,(stat* file))))
+                                                 (files (map (lambda (f)
+                                                               `(,f . ,(stat* (string-append file "/" f))))
+                                                             files)))))
+                                          (_ (list `(,file . ,(stat* file))))))
+                                      (lambda args
+                                        (let ((errno (system-error-errno args)))
+                                          (format (current-error-port) "~a: ~a~%"
+                                                  file (strerror errno))
+                                          '()))))
+                                  files)))
+           (dot-file? (lambda (x) (let ((file (car x)))
+                                    (and (string-prefix? "." file)
+                                         (not (string-prefix? "./" file))
+                                         (not (string-prefix? "../" file))))))
+           (files (if (or all? directory?) files
+                      (filter (negate dot-file?) files)))
+           (files (if (not sort-by-modification-time?) files
+                      (reverse (sort files file-modification-time<?))))
+           (files (if (not reverse?) files
+                      (reverse files))))
+      (cond (long?
+             (for-each (match-lambda
+                         ((f . st)
+                          (when inode?
+                            (format #t "~a " (stat:ino st)))
+                          (display-file f #:classify? classify?)
+                          (newline)))
+                       files))
+            (one-file-per-line?
+             (for-each (match-lambda
+                         ((f . st)
+                          (when inode?
+                            (format #t "~a " (stat:ino st)))
+                          (display f)
+                          (when classify?
+                            (display (file-class st)))
+                          (newline)))
+                       files))
+            (else
+             (display-tabulated (map (match-lambda
+                                       ((f . st)
+                                        (cond (inode?
+                                               (format #f "~a ~a" (stat:ino st) f))
+                                              (classify?
+                                               (format #f "~a~a" f (file-class st)))
+                                              (else f))))
+                                     files)))))))
 
 (define main ls)
